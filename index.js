d732f8f0c8084b27301c716342d7dc7b00c36a2e
Aleksei Nagovitsyn <al3x.nag@gmail.com>
2024-12-11 23:23:08 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import Fastify from "fastify";
+import proxy from "@fastify/http-proxy";
+
+const app = Fastify();
+
+app.register(proxy, {
+  upstream: "https://esm.sh",
+});
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
+
+// app.listen({ port: 3000 });
