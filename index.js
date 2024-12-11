b405be9834bc04478a940861b0cf34d598f1d18f
Aleksei Nagovitsyn <al3x.nag@gmail.com>
2024-12-11 23:42:51 +0500

    update

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.register(proxy, {
   upstream: "https://esm.sh",
 });
 
-export default async (req, res) => {
+export default async function handler(req, res) {
   await app.ready();
   app.server.emit("request", req, res);
-};
+}
 
 // app.listen({ port: 3000 });
