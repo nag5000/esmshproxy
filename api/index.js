01164939ec8f6f348c4bda0f46fa504b97293f84
Aleksei Nagovitsyn <al3x.nag@gmail.com>
2024-12-12 00:25:44 +0500

    handle redirects

--- api/index.js
+++ api/index.js
@@ -7,11 +7,24 @@ const app = Fastify();
 
 app.register(proxy, {
   upstream: "https://esm.sh",
+  replyOptions: {
+    onResponse: (req, reply, res) => {
+      // handle redirects
+      if (
+        [301, 302, 303, 307].includes(res.statusCode) &&
+        res.headers["location"]
+      ) {
+        reply.redirect(res.headers["location"]);
+      } else {
+        reply.send(res);
+      }
+    },
+  },
 });
 
-export default async function handler(req, res) {
+export default async function handler(req, reply) {
   await app.ready();
-  app.server.emit("request", req, res);
+  app.server.emit("request", req, reply);
 }
 
 // app.listen({ port: 3000 });
